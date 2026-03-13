82606c8a899b36e71a0a3684331cc3194153866e
Gibran_Leon <gibranleonlinux@gmail.com>
2026-03-12 18:00:49 -0700

    Add Practica1

--- practica1/cola_impresion.c
+++ practica1/cola_impresion.c
@@ -1,25 +1,33 @@
-// Practica 1: Cola de impresion en C
-// Inicio:  19/02/2026
-// Entrega: 13/03/2026
+#define MAX_USER 32
+#define MAX_DOC 48
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct ts_cola
+typedef enum
 {
-    int id;             // Numero autoincremental
-    char usuario[32];   // Nombre del usuario que mando a imprimir
-    char documento[42]; // Titulo del documento
-    int total_pgs;      // Numero total de paginas
-    int restante_pgs;   // Paginas restantes por imprimir
-    int copias;         // Copias por imprimir
-    int prioridad;      // NORMAL, URGENTE
-    int estado;         // EN_COLA, IMPRIMIENDO, COMPLETADO, CANCELADO
+    NORMAL = 0,
+    URGENTE = 1
+} Prioridad_t;
+
+typedef enum
+{
+    EN_COLA = 0,
+    IMPRIMIENDO = 1,
+    COMPLETADO = 2,
+    CANCELADO = 3
+} Estado_t;
+
+typedef struct
+{
+    int id;                  // autoincremental
+    char usuario[MAX_USER];  // quien imprime
+    char documento[MAX_DOC]; // nombre del documento
+    int paginas_total;       // total de paginas
+    int paginas_restantes;   // para simular progreso
+    int copias;              // opcional (>=1)
+    Prioridad_t prioridad;   // normal/urgente
 } cola;
 
 void menu();
-void q_init(cola cola_impresion, int llena);
+void q_init();
 void q_is_empty();
 void q_is_full();
 void q_enqueue();
@@ -35,15 +43,11 @@ int main()
 
 void menu()
 {
-    int llena = 0;
-    cola cola_impresion[10];
-
     int opcion;
 
     system("clear");
     do
     {
-
         printf("COLA DE IMPRESION\n");
         printf("1. Agregar\n");
         printf("2. Peek\n");
@@ -56,22 +60,13 @@ void menu()
         switch (opcion)
         {
         case 1:
-
-            if (llena == 1)
-            {
-                printf("LA LISTA ESTA LLENA Y NO SE PUEDEN AGREGAR DATOS");
-                break;
-            }
-            else
-            {
-                q_init(*cola_impresion, llena);
-            }
-
             break;
+
         case 4:
-            q_print(cola_impresion);
             break;
+
         case 5:
+            system("clear");
             printf("SALIENDO DEL PROGRAMA ... \n");
             break;
 
@@ -83,31 +78,10 @@ void menu()
     } while (opcion != 5);
 }
 
-void q_init(cola cola_impresion, int llena)
+void q_init(cola *cola_impresion)
 {
     int i = 0;
     char agreg[42];
-    do
-    {
-        if (i == 0)
-        {
-            printf("LA LISTA ESTA VACIA\n");
-            printf("Ingrese el nombre del documento: ");
-            scanf("%s", agreg);
-            strcpy(cola_impresion.documento, agreg);
-            printf("Se agrego el titulo a la cola N. %d\n", i);
-            i++;
-        }
-        else
-        {
-            printf("LA LISTA YA TIENE ELEMENTOS\n");
-            printf("Ingrese el nombre del documento: ");
-            scanf("%s", agreg);
-            strcpy(cola_impresion.documento, agreg);
-            printf("Se agrego el titulo a la cola N. %d", i);
-        }
-        i++;
-    } while (i < 10);
 }
 
 void q_print(cola *cola_impresion)
